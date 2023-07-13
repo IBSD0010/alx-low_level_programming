8f4891c1a81c1fabb20507367e770689433f639b
IBSD0010 <ibsd0010@gmail.com>
2023-07-13 10:17:27 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
  * @text_content: the string to add to the end of the file
  * Return: if the function fails or filename is NULL - -1
  * if the file does not exist the user lacks write permissions - -1
- * Otherwise -1
+ * Otherwise - 1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
